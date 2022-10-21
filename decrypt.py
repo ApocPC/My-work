1f95ddd2aef705f9f8aecb1dfff7a3dc5d815642
Adam Avenenti <110053918+ApocPC@users.noreply.github.com>
2022-10-21 11:48:40 -0500

    Add files via upload
    This is my first major project I've completed. I thought of building something unique that would potentially help me get a job. It's my own little encrypting/decrypting project which is pretty much just string replacement with added patterns for removing and replacing certain characters.

---
+++ decrypt.py
@@ -0,0 +1,170 @@
+import random
+
+decrypt_dict = {
+    "k": "b",
+    "s": "k",
+    "c": "s",
+    "l": "c",
+    "t": "l",
+    "d": "t",
+    "m": "d",
+    "v": "m",
+    "f": "v",
+    "n": "f",
+    "w": "n",
+    "g": "w",
+    "p": "g",
+    "x": "p",
+    "h": "x",
+    "q": "h",
+    "y": "q",
+    "j": "y",
+    "r": "j",
+    "z": "r",
+    "b": "z",
+    "&": "`",
+    "$": "&",
+    ">": "$",
+    "^": ">",
+    "#": "^",
+    "<": "#",
+    "%": "<",
+    "@": "%",
+    "|": "@",
+    ":": "|",
+    '"': ":",
+    "=": '"',
+    "+": "=",
+    "?": "+",
+    "_": "?",
+    "/": "_",
+    "'": "/",
+    "!": "'",
+    "\\": "!",
+    "-": "\\",
+    ".": "-",
+    "*": ".",
+    ";": "*",
+    ",": ";",
+    "`": ",",
+    "}": "(",
+    "[": "}",
+    ")": "[",
+    "{": ")",
+    "]": "{",
+    "(": "]",
+}
+encrypt_dict = {
+    "b": "k",
+    "k": "s",
+    "s": "c",
+    "c": "l",
+    "l": "t",
+    "t": "d",
+    "d": "m",
+    "m": "v",
+    "v": "f",
+    "f": "n",
+    "n": "w",
+    "w": "g",
+    "g": "p",
+    "p": "x",
+    "x": "h",
+    "h": "q",
+    "q": "y",
+    "y": "j",
+    "j": "r",
+    "r": "z",
+    "z": "b",
+    "`": "&",
+    "&": "$",
+    "$": ">",
+    ">": "^",
+    "^": "#",
+    "#": "<",
+    "<": "%",
+    "%": "@",
+    "@": "|",
+    "|": ":",
+    ":": '"',
+    '"': "=",
+    "=": "+",
+    "+": "?",
+    "?": "_",
+    "_": "/",
+    "/": "'",
+    "'": "!",
+    "!": "\\",
+    "\\": "-",
+    "-": ".",
+    ".": "*",
+    "*": ";",
+    ";": ",",
+    ",": "`",
+    "(": "}",
+    "}": "[",
+    "[": ")",
+    ")": "{",
+    "{": "]",
+    "]": "(",
+}
+vowel_dict = {
+    "1": "a",
+    "2": "e",
+    "3": "i",
+    "4": "o",
+    "5": "u",
+    "1": "A",
+    "2": "E",
+    "3": "I",
+    "4": "O",
+    "5": "U",
+}
+space_dict = {"6": " ", "7": " ", "8": " ", "9": " "}
+
+with open("char.txt", "r") as char_txt:
+    text_file = str(char_txt.read())
+    text = [vowel_dict.get(index, index).lower() for index in text_file]
+    text = [encrypt_dict.get(k, k) for k in text]
+
+
+def decrypt():
+    # First we will place back all the vowels removed
+    # As well as placing the spaces back in the user input
+    user_input = input("Enter the sentence or phrase you wish to decrypt:")
+    user_input_2 = (vowel_dict.get(x, x).lower() for x in user_input)
+    msg = (space_dict.get(n, n) for n in user_input_2)
+    decrypted_sentence = step_2(msg)
+    # Layer 2 will focus on replacing the original characters
+    print(f"Decrypted data: {decrypted_sentence}")
+
+
+def step_2(msg):
+    offset = 0
+    cycle = 0
+    final = ""
+    decrypted_sentence = ""
+    text_index = 0
+    for character in msg:
+        if character != " ":
+            if offset == cycle:
+                # Here we will replace the random vowels chosen with the original characters
+                # that were provided by the user's input. The text file contains the
+                # characters removed and the order in which they are to be placed back in
+                final += text[text_index]
+                text_index += 1
+            else:
+                final += character
+            cycle += 1
+            if cycle == 5:
+                cycle = 0
+                offset += 1
+            if offset == 5:
+                offset = 0
+        else:
+            final += " "
+    # This will match the keys with their value pairs
+    decrypted_sentence = "".join(decrypt_dict.get(c, c) for c in final)
+    return decrypted_sentence
+
+decrypt()
---
+++ encrypt.py
@@ -0,0 +1,120 @@
+import random
+
+vowels = "AEIOUaeiou"
+spaces = "6789"
+encrypt_dict = {
+    "b": "k",
+    "k": "s",
+    "s": "c",
+    "c": "l",
+    "l": "t",
+    "t": "d",
+    "d": "m",
+    "m": "v",
+    "v": "f",
+    "f": "n",
+    "n": "w",
+    "w": "g",
+    "g": "p",
+    "p": "x",
+    "x": "h",
+    "h": "q",
+    "q": "y",
+    "y": "j",
+    "j": "r",
+    "r": "z",
+    "z": "b",
+    "`": "&",
+    "&": "$",
+    "$": ">",
+    ">": "^",
+    "^": "#",
+    "#": "<",
+    "<": "%",
+    "%": "@",
+    "@": "|",
+    "|": ":",
+    ":": '"',
+    '"': "=",
+    "=": "+",
+    "+": "?",
+    "?": "_",
+    "_": "/",
+    "/": "'",
+    "'": "!",
+    "!": "\\",
+    "\\": "-",
+    "-": ".",
+    ".": "*",
+    "*": ";",
+    ";": ",",
+    ",": "`",
+    "(": "}",
+    "}": "[",
+    "[": ")",
+    ")": "{",
+    "{": "]",
+    "]": "(",
+    " ": random.choice(spaces),
+}
+vowel_dict = {
+    "a": "1",
+    "e": "2",
+    "i": "3",
+    "o": "4",
+    "u": "5",
+    "A": "1",
+    "E": "2",
+    "I": "3",
+    "O": "4",
+    "U": "5",
+}
+# Step 1 is the replacement of all vowels in the user input
+def encrypt():
+    user_input = input("Enter the sentence or phrase you wish to encrypt:")
+    msg = [vowel_dict.get(x, x) for x in user_input]
+    encrypted_message = step_2(msg)
+    print(f"Encrypted Data: {encrypted_message}")
+
+
+def step_2(msg):
+    # Layer 2 will deal with choosing a character and replacing it with a vowel.
+    # This layer replaces 1 character with a vowel for every 5 characters in a row.
+    # The pattern for chosing a variable is as follows:
+    # their  =>  # (A)heir
+    # there  =>  # t(U)ere
+    # throw  =>  # th(I)re
+    # mercy  =>  # mer(O)y
+    # raven  =>  # rave(E)
+    text_file = ""  # Character Text File
+    vowel_sen = ""  # New vowel string
+    encrypted_message = ""
+    offset = 0
+    cycle = 0
+    for character in msg:
+        if character != " ":
+            if offset == cycle:
+                vowel_sen += random.choice(vowels)
+                text_file += character
+            else:
+                vowel_sen += character
+            cycle += 1
+            if cycle == 5:
+                cycle = 0
+                offset += 1
+            if offset == 5:
+                offset = 0
+        else:
+            vowel_sen += " "
+    # Layer 2 also deals with replacing all other characters within the user input
+    # Replacing the letters within the user input
+    # Replacing the symbols and punctuations
+    # Replacing the spaces
+    encrypted_message = "".join(encrypt_dict.get(c, c) for c in vowel_sen)
+    # Here a text file will hold the replaced characters
+    char_txt = open("char.txt", "w")
+    char_txt.writelines(text_file)
+    # This print statement keeps track of the characters removed
+    print(f"Characters replaced by vowels : {text_file}")
+    return encrypted_message
+encrypt()
